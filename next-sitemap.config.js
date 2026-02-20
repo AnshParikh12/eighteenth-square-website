e7cb547a55a8d2bdaee883550f2e8b6ee4045a3a
AnshParikh12 <anshparikh1205gmail.com>
2026-02-20 15:14:01 -0500

    add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://eighteenthsquare.in', // Your new domain
+  generateRobotsTxt: true, // optional: generates robots.txt
+  sitemapSize: 5000, // optional
+  changefreq: 'weekly', // optional
+  priority: 0.7, // optional
+}
